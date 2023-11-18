759a7ae737789943a5511c6f985d83f31b4d87e3
Richard Körösi <korosi.richard@gmail.com>
2023-11-18 15:03:21 +0100

    Uvod commit

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
-# This is a sample Python script.
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from sklearn.decomposition import PCA
+from sklearn.ensemble import RandomForestRegressor as rfr
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.svm import SVR
+from sklearn.tree import DecisionTreeRegressor as dtr
+from sklearn.tree import plot_tree
+from tabulate import tabulate
+from yellowbrick.regressor import ResidualsPlot
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# ZDROJE KU KODOM ------------------------------------------------------------------------------------------------------
+# ======================================================================================================================
+# Zdrojove kody z cviceni (dostupne na dokumentovom serveri AIS):
+#   Autor: Ing. Vanesa Andicsová
+#   Subory:
+#       seminar2.py
+# Zdrojove kody boli vyuzite napriklad pre vypisy do konzoly, vytvorenie zakladnych grafov,
+# ktore sme mali za ulohu  vypracovat
+# Taktiez kody boli vyuzite pri zakladnom nastavovani vstupnych/vystupnych dat (X,y) a pri zakladnom nastavovani modelu
+# ======================================================================================================================
+# Grafy, Pomocne funkcie, Casti funkcii...:
+#  Autor/Spoluautor: Github Copilot
+#  Grafy, pomocne funkcie a casti funkcii boli vypracoavane za pomoci Github Copilota
+# ======================================================================================================================
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Uvod -----------------------------------------------------------------------------------------------------------------
+# Uvod bol inspirovany zdrojovim kodom seminar2.py (vid. ZDROJE KU KODOM)
+pd.options.display.width = None
+pd.options.display.max_columns = None
+pd.set_option('display.max_rows', 3000)
+pd.set_option('display.max_columns', 3000)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+pd.set_option('mode.chained_assignment', None)
