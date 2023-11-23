88453e733f4b28ae1a7c75f1d8ac883e7292296b
Richard Körösi <korosi.richard@gmail.com>
2023-11-23 08:12:40 +0100

    xdd

--- main.py
+++ main.py
@@ -2,32 +2,64 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
-from keras.preprocessing.image import ImageDataGenerator
-from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D
-from keras.layers import Activation, Dropout, Flatten, Dense
-from keras import backend as K
+import pathlib
+import os
+import PIL.Image
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import math
 
-# ZDROJE KU KODOM ------------------------------------------------------------------------------------------------------
-# ======================================================================================================================
-# Zdrojove kody z cviceni (dostupne na dokumentovom serveri AIS):
-#   Autor: Ing. Vanesa Andicsová
-#   Subory:
-#       seminar2.py
-# Zdrojove kody boli vyuzite napriklad pre vypisy do konzoly, vytvorenie zakladnych grafov,
-# ktore sme mali za ulohu  vypracovat
-# Taktiez kody boli vyuzite pri zakladnom nastavovani vstupnych/vystupnych dat (X,y) a pri zakladnom nastavovani modelu
-# ======================================================================================================================
-# Grafy, Pomocne funkcie, Casti funkcii...:
-#  Autor/Spoluautor: Github Copilot
-#  Grafy, pomocne funkcie a casti funkcii boli vypracoavane za pomoci Github Copilota
-# ======================================================================================================================
+img_size = 150
+batch_size = 32
 
+# Notebook
+base_dir = 'C:/Users/koros/Desktop/SUNS/Zadania/Zadanie3/SUNS-Zadanie3/data'
 
-# Uvod -----------------------------------------------------------------------------------------------------------------
-# Uvod bol inspirovany zdrojovim kodom seminar2.py (vid. ZDROJE KU KODOM)
-pd.options.display.width = None
-pd.options.display.max_columns = None
-pd.set_option('display.max_rows', 3000)
-pd.set_option('display.max_columns', 3000)
-pd.set_option('mode.chained_assignment', None)
+# PC
+# base_dir = 'C:/Users/koros/Desktop/SUNS/Zadania/Zadanie3/SUNS-Zadanie3/data'
+
+train_dir = os.path.join(base_dir, 'train')
+test_dir = os.path.join(base_dir, 'test')
+
+animals_folders = list(pathlib.Path(train_dir).glob('*'))
+
+# Calculate the grid size based on the number of animal folders
+grid_size = math.ceil(math.sqrt(len(animals_folders)))
+
+fig = plt.figure(figsize=(10, 10))  # Define the figure size
+
+for i, animal_folder in enumerate(animals_folders):
+    animal_images = list(animal_folder.glob('*'))
+    im = PIL.Image.open(str(animal_images[0]))
+    
+    ax = fig.add_subplot(grid_size, grid_size, i+1)  # Add a subplot for each image
+    ax.imshow(im)
+    ax.axis('off')  # Hide axes
+
+plt.show()
+
+train_ds = tf.keras.utils.image_dataset_from_directory(
+  train_dir,
+  shuffle=True,
+  validation_split=0.1,
+  subset="training",
+  seed=123,
+  image_size=(img_size, img_size),
+  batch_size=batch_size)
+
+val_ds = tf.keras.utils.image_dataset_from_directory(
+  train_dir,
+  validation_split=0.1,
+  subset="validation",
+  seed=123,
+  image_size=(img_size, img_size),
+  batch_size=batch_size)
+
+test_ds = tf.keras.utils.image_dataset_from_directory(
+  test_dir,
+  seed=123,
+  image_size=(img_size, img_size),
+  batch_size=batch_size)
+
+class_names = train_ds.class_names
+print(class_names)
