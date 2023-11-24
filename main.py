c21d2f4c415d547339c2e05f5eaa841e452d9b82
Richard Körösi <korosi.richard@gmail.com>
2023-11-24 15:57:38 +0100

    pain

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ import seaborn as sns
 import pathlib
 import os
 import PIL.Image
+from keras.src.applications import ResNet50
+from keras.src.applications.convnext import preprocess_input, decode_predictions
 from sklearn.metrics import confusion_matrix
-import tensorflow as tf
-import matplotlib.pyplot as plt
 import math
+import keras
 
 # base_dir = 'C:/Users/koros/Desktop/SUNS/Zadania/Zadanie3/SUNS-Zadanie3/data'
-base_dir = 'C:/Users/koros/Desktop/SUNS/Zadania/Zadanie3/SUNS-Zadanie3/data'
+base_dir = 'D:/Skola/ING/SEM1/SUNS/SUNS-Zadanie3/data'
 train_dir = os.path.join(base_dir, 'train')
 test_dir = os.path.join(base_dir, 'test')
 animals_folders = list(pathlib.Path(train_dir).glob('*'))
@@ -19,79 +20,64 @@ animals_folders = list(pathlib.Path(train_dir).glob('*'))
 img_size = 224
 batch_size = 32
 
-def initialize_data(i_size, b_size, train_d, test_d):
-    train_ds = tf.keras.utils.image_dataset_from_directory(
-    train_d,
-    shuffle=True,
-    validation_split=0.1,
-    subset="training",
-    seed=123,
-    image_size=(i_size, i_size),
-    batch_size=b_size)
 
-    val_ds = tf.keras.utils.image_dataset_from_directory(
-    train_d,
-    validation_split=0.1,
-    subset="validation",
-    seed=123,
-    image_size=(i_size, i_size),
-    batch_size=b_size)
+def initialize_data():
+    print("Train data: ")
+    train_das = keras.utils.image_dataset_from_directory(
+        train_dir,
+        shuffle=True,
+        validation_split=0.1,
+        subset="training",
+        seed=123,
+        image_size=(img_size, img_size),
+        batch_size=batch_size)
 
-    test_ds = tf.keras.utils.image_dataset_from_directory(
-    test_d,
-    seed=123,
-    image_size=(i_size, i_size),
-    batch_size=b_size)
+    print("Validation data: ")
+    val_das = keras.utils.image_dataset_from_directory(
+        train_dir,
+        validation_split=0.1,
+        subset="validation",
+        seed=123,
+        image_size=(img_size, img_size),
+        batch_size=batch_size)
 
-    class_names = train_ds.class_names
+    print("Test data: ")
+    test_das = keras.utils.image_dataset_from_directory(
+        test_dir,
+        seed=123,
+        image_size=(img_size, img_size),
+        batch_size=batch_size)
+
+    print("Class names: ")
+    class_names = train_das.class_names
     print(class_names)
 
-    return train_ds, val_ds, test_ds
-
-def show90animals(anim_folders):
-  # Calculate the grid size based on the number of animal folders
-  grid_size = math.ceil(math.sqrt(len(anim_folders)))
-
-  fig = plt.figure(figsize=(10, 10))  # Define the figure size
-
-  for i, animal_folder in enumerate(anim_folders):
-      animal_images = list(animal_folder.glob('*'))
-      im = PIL.Image.open(str(animal_images[0]))
-      
-      ax = fig.add_subplot(grid_size, grid_size, i+1)  # Add a subplot for each image
-      ax.imshow(im)
-      ax.axis('off')  # Hide axes
-
-  plt.show()
-  return None
+    return train_das, val_das, test_das
 
 
-def imagenet_prediction(test_ds):
-    # Load the pre-trained ResNet50 model
+def show90animals():
+    # Calculate the grid size based on the number of animal folders
+    grid_size = math.ceil(math.sqrt(len(animals_folders)))
+
+    fig = plt.figure(figsize=(10, 10))  # Define the figure size
+
+    for i, animal_folder in enumerate(animals_folders):
+        animal_images = list(animal_folder.glob('*'))
+        im = PIL.Image.open(str(animal_images[0]))
+
+        ax = fig.add_subplot(grid_size, grid_size, i + 1)  # Add a subplot for each image
+        ax.imshow(im)
+        ax.axis('off')  # Hide axes
+
+    plt.show()
+    return None
+
+
+def test_imagenet_model_on_test_data():
     model = ResNet50(weights='imagenet')
-
-    # Initialize lists to store the predictions and true labels
-    y_pred = []
-    y_true = []
-
-    # Iterate over the test dataset
-    for images, labels in test_ds:
-        # Preprocess the images
-        images = preprocess_input(images)
-
-        # Make predictions
-        preds = model.predict(images)
-
-        # Decode the predictions
-        decoded_preds = decode_predictions(preds, top=1)
-
-        # Append the predictions and true labels to the lists
-        y_pred.extend(decoded_preds)
-        y_true.extend(labels.numpy())
+    
+    return None
 
 
-    return y_true, y_pred
-  
-train_ds, val_ds, test_ds = initialize_data(img_size, batch_size, train_dir, test_dir)
-initialize_data(img_size, batch_size, train_dir, test_dir)
-imagenet_prediction(test_ds)
+train_ds, val_ds, test_ds = initialize_data()
+show90animals()
